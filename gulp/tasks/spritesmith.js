6313f1b2725d3dbf9480ca3c39cd99cd7a8f3f5e
Laikovskaya Valentina <esetnod1111@gmail.com>
2017-02-13 12:29:04 +0200

    first commit

---
+++ gulp/tasks/spritesmith.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function() {
+    $.gulp.task('spritesmith', function() {
+        return $.gulp.src('./source/images/sprite/*.*')
+                .pipe($.gp.spritesmith({
+                    imgName: 'sprite.png',
+                    cssName: 'sprite.css',
+                }))
+
+        .pipe($.gulp.dest('./build/images/'));
+        .pipe($.gulp.dest('./build/css/'));
+    });
+};
