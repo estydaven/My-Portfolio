2a8ef2891e02e824dbfd4493b8a7ca57723ae731
Laikovskaya Valentina <esetnod1111@gmail.com>
2017-02-16 16:49:14 +0200

    fifth

--- gulp/tasks/spritesmith.js
+++ gulp/tasks/spritesmith.js
@@ -5,10 +5,12 @@ module.exports = function() {
         return $.gulp.src('./source/images/sprite/*.*')
                 .pipe($.gp.spritesmith({
                     imgName: 'sprite.png',
-                    cssName: 'sprite.css',
+                    cssName: 'sprite.scss',
+                    cssFormat: 'css',
+                    padding: 50
                 }))
 
-        .pipe($.gulp.dest('./build/images/'))
-        .pipe($.gulp.dest('./build/css/'));
+            .pipe($.gulp.dest('./build/images/'))
+            .pipe($.gulp.dest('./build/assets/css/'));
     });
 };
