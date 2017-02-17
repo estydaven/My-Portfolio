c8f93f4c9c90f2fe6727bd02f4f06761c5ce5ee7
Laikovskaya Valentina <esetnod1111@gmail.com>
2017-02-17 14:35:53 +0200

    ninth

--- gulp/tasks/copy.font.js
+++ gulp/tasks/copy.font.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
     $.gulp.task('copy:font', function() {
-        return $.gulp.src('./source/fonts/**/*.*', { since: $.gulp.lastRun('copy:image') })
+        return $.gulp.src('./source/fonts/**/*.*', { since: $.gulp.lastRun('copy:font') })
             .pipe($.gulp.dest($.config.root + '/assets/fonts'));
     });
 };
